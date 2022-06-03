9997b181d6f1ba0730625bd0c6dc82ffe30f16b5
Amir Szekely <kichik@gmail.com>
2022-06-03 12:55:07 -0700

    test go release

--- .projenrc.js
+++ .projenrc.js
@@ -6,10 +6,29 @@ const project = new awscdk.AwsCdkConstructLibrary({
   defaultReleaseBranch: 'main',
   name: 'projen-deploy-key-test',
   repositoryUrl: 'https://github.com/kichik/projen-deploy-key-test.git',
+  description: 'Test repo for https://github.com/projen/projen/pull/1906',
 
   // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  publishToGo: {
+    moduleName: 'github.com/kichik/projen-deploy-key-test-go',
+    githubUseSsh: true,
+  },
+
+  depsUpgrade: false,
 });
+
+const releaseWorkflow = project.github.tryFindWorkflow('release');
+// skip npm as we are only testing golang
+releaseWorkflow.file.addDeletionOverride('jobs.release_npm');
+// inject our branch of publib
+releaseWorkflow.file.addOverride('jobs.release_golang.steps.8.run', `wget https://github.com/kichik/publib/archive/refs/heads/patch-1.tar.gz
+tar xzf patch-1.tar.gz
+cd publib-patch-1
+npm run build
+sudo npm i -g dist/js/publib-0.0.0.tgz
+publib-golang`);
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="projen-deploy-key-test.Hello"></a>
+
+#### Initializers <a name="Initializers" id="projen-deploy-key-test.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'projen-deploy-key-test'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#projen-deploy-key-test.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="projen-deploy-key-test.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
